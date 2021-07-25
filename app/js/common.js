20843dbd5bb59c3f2a4ba3c13629f11b744840d7
Viktoria <zaharova181114@gmail.com>
2021-07-25 22:22:02 +0300

    edit1

--- app/js/common.js
+++ app/js/common.js
@@ -1,24 +1,23 @@
-$(document).ready(function () {
-    $('.btn-arrow-toggle').on('click', function (e) {
-        e.preventDefault();
-        $('.footer-box h3').removeClass('click');
-        $('.footer-box__body').slideUp();
-        $(this).parent().toggleClass('click').siblings('.footer-box__body').slideToggle();
-    });
-
-    $('.menu-main__list li a').each(function () {
-        var location = window.location.href;
-        var link = this.href;
-        if (location === link) {
-            $(this).addClass('current');
-        }
-    });
+$('.btn-arrow-toggle').on('click', function (e) {
+    e.preventDefault();
+    $('.footer-box h3').removeClass('click');
+    $('.footer-box__body').slideUp();
+    $(this).parent().toggleClass('click').siblings('.footer-box__body').slideToggle();
 });
 
+$('.menu-main__list li a').each(function () {
+    var location = window.location.href;
+    var link = this.href;
+    if (location === link) {
+        $(this).addClass('current');
+    }
+});
+
+
 $('.btn-burger-mobile').on('click', function (e) {
-   e.preventDefault();
-   $(this).toggleClass('clicked');
-   $('.mobile-menu').fadeToggle();
+    e.preventDefault();
+    $(this).toggleClass('clicked');
+    $('.mobile-menu').fadeToggle();
 });
 
 $('.dropdown-item__links').on('click', function (e) {
