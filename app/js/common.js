febf211603f852559d8f6cc5ba55155515015b45
Viktoria <zaharova181114@gmail.com>
2021-09-04 11:41:56 +0300

    add filter

--- app/js/common.js
+++ app/js/common.js
@@ -153,3 +153,12 @@ $(document).ready(function() {
         allowClear: true,
     });
 });
+
+// load header+footer all pages
+$(function () {
+    var includes = $('[data-include]');
+    jQuery.each(includes, function () {
+        var file = 'views/' + $(this).data('include') + '.html';
+        $(this).load(file);
+    });
+});
