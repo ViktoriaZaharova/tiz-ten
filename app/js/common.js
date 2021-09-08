5cf0f35507c0f024e0d209a4ee61bf1ca82cfeac
Viktoria <zaharova181114@gmail.com>
2021-09-08 11:25:32 +0300

    add modal

--- app/js/common.js
+++ app/js/common.js
@@ -1,54 +1,3 @@
-$('.btn-arrow-toggle').on('click', function (e) {
-    e.preventDefault();
-    $('.footer-box h3').removeClass('click');
-    $('.footer-box__body').slideUp();
-    $(this).parent().addClass('click').siblings('.footer-box__body').slideDown();
-});
-
-// $('.menu-main__list li a').each(function () {
-//     var location = window.location.href;
-//     var link = this.href;
-//     if (location === link) {
-//         $(this).addClass('current');
-//     }
-// });
-
-$('.btn-catalog').on('click', function (e) {
-    e.preventDefault();
-    $('.overlay-menu').fadeToggle();
-    $(this).toggleClass('open-menu');
-    $('.menu-container').fadeToggle();
-});
-
-
-$('.btn-burger-mobile').on('click', function (e) {
-    e.preventDefault();
-    $(this).toggleClass('clicked');
-    $('.mobile-menu').fadeToggle();
-});
-
-$('.dropdown-item__links').on('click', function (e) {
-    e.preventDefault();
-    $(this).siblings('.dropdown-menu').css('left', '0');
-});
-
-$('.prev-menu').on('click', function (e) {
-    e.preventDefault();
-    $(this).parent('.dropdown-menu').css('left', '-100%');
-});
-
-$(".mega-menu__route").hover(function () {
-    var id = $(this).attr('data-tab'),
-        content = $('.js-tab-content[data-tab="'+ id +'"]');
-
-    $('.mega-menu__route.active').removeClass('active'); // 1
-    $(this).addClass('active'); // 2
-
-    $('.js-tab-content.active').removeClass('active'); // 3
-    content.addClass('active'); // 4
-});
-
-
 // accordeon
 function accordeon() {
     var panel = $('.panel_heading');
@@ -124,18 +73,7 @@ $('.breadcrumbs-button').on('click', function () {
    $('.breadcrumb').toggleClass('open');
 });
 
-// input search
-$(function () {
-    $('.form-search input').keydown(checkInput).keyup(checkInput);
-});
 
-function checkInput() {
-    if ($('.form-search input').val() === '') {
-        $(this).parents('.form-search-wrap').removeClass('clicked');
-    } else {
-        $(this).parents('.form-search-wrap').addClass('clicked');
-    }
-}
 
 // событие клика за пределами блока
 $(document).mouseup(function (e){ // событие клика по веб-документу
